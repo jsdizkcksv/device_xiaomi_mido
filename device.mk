c2f602ac71a9bc25641904634b69f22c392fc6f1
Tim Zimmermann <tim@linux4.de>
2022-06-13 09:19:40 +0530

    mido: Flash dummy super image if needed
    * Needed because stock does not use dynamic partitions so
      there's no existing metadata that could be used
    
    Signed-off-by: P-Salik <peersalik16@gmail.com>
    Signed-off-by: Nalinish Ranjan <nalinishranjan05@gmail.com>

--- device.mk
+++ device.mk
@@ -184,6 +184,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
 
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi_mido
